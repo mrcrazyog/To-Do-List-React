5a6233fc7f18f1fb2f2ffbdab0d33f381fcd3d18
mrcrazyog <75265930+mrcrazyog@users.noreply.github.com>
2022-04-21 21:09:39 +0200

    git init

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  function handleClick() {}
+
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li> {props.text} </li>
+    </div>
+  );
+}
+
+export default ToDoItem;
